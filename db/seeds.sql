3da58b5d96ffaee39173651653caa0eec521eac2
Harmin Jeong <harminjeong@Harmins-MacBook-Pro.local>
2018-01-23 02:00:29 -0500

    initial site files

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name)
+VALUES ('Cheeseburger');
+
+INSERT INTO burgers (burger_name)
+VALUES ('Turkey Burger');
+
+INSERT INTO burgers (burger_name)
+VALUES ('Veggie Burger');
+
+INSERT INTO burgers (burger_name)
+VALUES ('Salmon Burger');
